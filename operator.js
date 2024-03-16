18a2fc78a3c3d0d86667edb5660a0df3ab964da2
imranFRMU <162621539+imranFRMU@users.noreply.github.com>
2024-03-16 13:33:46 -0700

    Add files via upload

---
+++ operator.js
@@ -0,0 +1,110 @@
+// arthmetic operator
+// console.log(2+9) 
+// let num1 : number = 2
+// let num2 : number = 4
+// let answer = 4+2
+// console.log (answer)
+// let num1 : number = 4
+//let num2 : number = 2
+//let answer = 4-2
+//console.log (answer)
+// let num1 = 5
+// num1 = num1 + 1 /8 /9
+// console.log(num1)
+// let num1 = 5
+// num1++ /--
+// console.log(num1)
+// let n1 = 10
+// let n2 = n1 +1
+// console.log (n2)
+// let num1 = 3
+// num1 *= 3
+// console.log(num1)
+// let n1 = 3
+// console.log (n1 == 3)
+// let n1 = 3
+// console.log (n1 == 1)
+// let n1 = 3
+// console.log (n1 === 1)
+// let n1 = 3
+// console.log (n1 === "1")
+// let n1 = Ima
+// let num1 = 2
+// let num2 = 5
+// let answer = 5+2
+// console.log(answer)
+// let num1 = 2
+// let num2 = 5
+// console.log(2+5)
+// let num1 = 2
+// let num2 = 5
+// console.log(2-5)
+// let num1 = 2
+// let num2 = 5
+// console.log(2*5)
+// let num1 = 2
+// let num2 = 6
+// console.log(6/2)
+// let num1 = 10
+// num1 = num1 + 1 
+// console.log(num1)
+// let num1 = 10
+// num1 = num1 - 1 
+// console.log(num1)
+// let num1 = 10
+// num1 = num1 * 10 
+// console.log(num1)
+// let num1 = 10
+// num1 = num1 / 100 
+//console.log(num1)
+// let num1 = 5
+// num1++
+// console.log(num1)
+// let num1 = 5
+// num1--
+// console.log(num1)
+// let n1 = 10
+// let n2 = n1 +1
+// console.log (n2)
+//  let num1 = 3
+//  num1 *=3
+//  console.log(num1)
+// let num1 = 3
+//  num1 /=3
+//  console.log(num1)
+// let n1 = 3
+// console.log (n1 == 3)
+// let n1 = 3
+//  console.log (n1 == 1)
+// let n1 = 3
+// console.log (n1 === 1)
+// let n1 = 3
+// console.log (n1 === "1")
+// let n1 = 3
+// console.log (n1 == 3)
+// let n1 = 3
+// console.log (n1 == 1)
+// let n1 = 3
+// console.log (n1 == 1)
+// let n1 = 3
+// console.log (n1 === 1)
+// let num1 : number = 2
+// let num2 : number = 4
+// let answer = 4+2
+// console.log (answer)
+// let n1 = 3
+// console.log (n1 === 1)
+// let num9= 3
+// num9+= 3
+// let n1= 3
+// console.log(n1===3 && n1===2)
+// let num1= 3
+// console.log(num1===3 && num1>2)
+// let num1= 3
+// console.log(num1===3 && num1<2)
+//  let n1= 3
+//  console.log(n1===3 || n1>2)
+// let n1= 3
+// console.log(n1===3 || n1<2)
+var n1 = 3;
+console.log(n1 === 3 || n1 < 2);
